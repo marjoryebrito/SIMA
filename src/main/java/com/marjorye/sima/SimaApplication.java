7b0f09a3aafd422953bc50c122932c1d2f676bd6
marjoryeads <marjorye.ads@gmail.com>
2021-09-10 17:46:31 -0300

    Initial commit

---
+++ src/main/java/com/marjorye/sima/SimaApplication.java
@@ -0,0 +1,13 @@
+package com.marjorye.sima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marjorye/sima/SimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marjorye.sima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
